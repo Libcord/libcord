0c3b8bde2cde9ac671e3ac807f43ea99718ec6eb
tovade <gamingethan650@gmail.com>
2021-08-27 21:11:42 +0200

    feat(Role): finish structure

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,12 @@
 import { Client } from "../src/index";
-
+import { token } from "../config.json";
 const client = new Client();
 
-client.connect("");
+client.connect(token);
 client.on("ready", () => {
   console.log(client.user?.username);
 });
 client.on("messageCreate", (message) => {
   if (message.author?.bot) return;
-  message.channel.send(`${client.ws.ping}`);
+  console.log(message.member.displayName);
 });
