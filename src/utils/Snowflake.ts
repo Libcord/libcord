8c0f235149a098826100c062efbd9d94a74305a4
Kamil G <59805849+mafineeek@users.noreply.github.com>
2021-10-03 21:00:55 +0200

    Update Snowflake.ts

--- src/utils/Snowflake.ts
+++ src/utils/Snowflake.ts
@@ -1,4 +1,4 @@
-export type Snowflake = `${bigint}`;
+export type Snowflake = bigint;
 
 export function getDate(snowflake: Snowflake): number {
     return Math.floor(parseInt(snowflake, 10) / 4194304) + 1420070400000;
